025609ccefef3d57c99a8e496cec9ad0b4a27bcb
Eswarsainath <eswarsainath3@gmail.com>
2021-12-04 23:13:16 -0500

    Updtaed benchmark program

--- src/main/java/edu/neu/coe/info6205/finalProject/sort/BenchMarkForAllSort.java
+++ src/main/java/edu/neu/coe/info6205/finalProject/sort/BenchMarkForAllSort.java
@@ -33,7 +33,7 @@ public class BenchMarkForAllSort {
 
     }
 
-    static void performBenchmark(String description, Consumer<String[]> fun, Supplier<String[]> supplier, int m){
+    static void runBenchmark(String description, Consumer<String[]> fun, Supplier<String[]> supplier, int m){
 
         Benchmark<String[]> benchmark = new Benchmark_Timer<>(description, fun);
         double time = benchmark.runFromSupplier(supplier,m);
@@ -43,11 +43,47 @@ public class BenchMarkForAllSort {
 
     public static void main(String[] args) {
         
-        DualPivotQuickSort dp = new DualPivotQuickSort(new ChineseComparator());
+        DualPivotQuickSort dp = new DualPivotQuickSort(new TeluguComparator());
+        String description = "Benchmark of dual pivot quick sort with 1 million random order telugu words";
+        runBenchmark(description,dp::dualPivotQuickSort,BenchMarkForAllSort::teluguWordsFromFile,5);
         //performBenchmark("dual pivot sort for chinese words", dp::dualPivotQuickSort,BenchMarkForAllSort::chineseWordsFromFile,1);
 
+        DualPivotQuickSort dpChinese = new DualPivotQuickSort(new ChineseComparator());
+        String descr = "Benchmark of dual pivot quick sort with 1 million random order chinese words";
+        runBenchmark(descr, dpChinese::dualPivotQuickSort, BenchMarkForAllSort:: chineseWordsFromFile, 5);
+
         TimSort tm = new TimSort(new ChineseComparator());
-        //performBenchmark("Timsort for chinese words", tm::dualPivotQuickSort,BenchMarkForAllSort::chineseWordsFromFile,1);
+        String descriptionTim = "Benchmark of Tim sort with 1 million random order chinese words";
+        runBenchmark(descriptionTim,tm::sort,BenchMarkForAllSort::chineseWordsFromFile,5);
+
+
+        TimSort tmTelugu = new TimSort(new TeluguComparator());
+        String descriptionTimTelugu = "Benchmark of Tim sort with 1 million random order chinese words";
+        runBenchmark(descriptionTimTelugu,tmTelugu::sort,BenchMarkForAllSort::teluguWordsFromFile,5);
+
+
+        String descrTelugu = "Benchmark of MSD Radix sort with 1 million random order telugu words";
+        runBenchmark(descrTelugu,MSDRadixSort::sort,BenchMarkForAllSort::teluguWordsFromFile,5);
+
+        String descrChineseMSD = "Benchmark of MSD Radix sort with 1 million random order telugu words";
+        runBenchmark(descrChineseMSD,MSDRadixSortChinese::sort,BenchMarkForAllSort::chineseWordsFromFile,5);
+
+        LSDRadixSort lsdTelugu = new LSDRadixSort();
+        String descrTeluguLsd = "Benchmark of LSD Radix sort with 1 million random order telugu words";
+        runBenchmark(descrTeluguLsd, lsdTelugu::sort,BenchMarkForAllSort::teluguWordsFromFile,5);
+
+        LSDRadixSortChinese lsdRadixSortChinese = new LSDRadixSortChinese();
+        String descrLSDChinese = "Benchmark of LSD Radix sort with 1 million random order chinese words";
+        runBenchmark(descrLSDChinese, lsdRadixSortChinese::sort,BenchMarkForAllSort::chineseWordsFromFile,5);
+
+
+
+
+
+
+
+
+
 
     }
 }
